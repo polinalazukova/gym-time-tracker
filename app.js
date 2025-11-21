40b814ed280b5d15de9042599c6918294589dc3c
polinalazukova <lazukovapolina75@gmail.com>
2025-11-21 18:02:09 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function checkIn() {
+    document.getElementById('status').textContent = 'Начало тренировки';
+}
+
+function checkOut() {
+    document.getElementById('status').textContent = 'Конец тренировки';
+}
